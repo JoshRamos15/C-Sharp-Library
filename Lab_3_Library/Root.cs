ca2975189150f0ab3e7eacde6f6489c02240f7ee
JoshRamos15 <113556514+JoshRamos15@users.noreply.github.com>
2024-02-18 22:14:32 -0500

    Add files via upload

---
+++ Lab_3_Library/Root.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab_3_Library
+{
+    public class AverageCalculator
+    {
+        public static double Avg(double a, double b)
+        {
+            return (a + b) / 2;
+        }
+
+        public static double Avg(double a, double b, double c)
+        {
+            return (a + b + c) / 3;
+        }
+    }
+}
